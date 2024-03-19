e2c467efa9ba86cb83a5b5af7339c8b84396d01b
s <steffen.samberger@gmail.com>
2024-03-19 17:22:49 +0100

    a ‘hacky’ workaround

--- src/generateSchema.ts
+++ src/generateSchema.ts
@@ -2,7 +2,10 @@ import { writeFileSync } from 'fs'
 import { zodToJsonSchema } from 'zod-to-json-schema'
 import { FunctionInputSchema } from './automateFunction.js'
 
-const schema = zodToJsonSchema(FunctionInputSchema)
+const schema = zodToJsonSchema(FunctionInputSchema, {target: "jsonSchema2019-09"})
+
+schema.$schema = "https://json-schema.org/draft/2020-12/schema"; // a ‘hacky’ workaround
+
 
 const args = process.argv.slice(2)
 
